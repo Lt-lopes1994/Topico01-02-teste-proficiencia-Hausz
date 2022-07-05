fe72a7fcbd0f877f07e9b4a839b145b8cabf27e8
Bruno Mantovan Lopes <brunomantovanlopes@gmail.com>
2022-07-05 13:47:52 -0300

    COREÇÃO: DB importada corretamente

--- src/scripts/conection.js
+++ src/scripts/conection.js
@@ -4,11 +4,11 @@ env.config();
 const knex = require("knex")({
   client: "pg",
   connection: {
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
+    host: process.env.PG_HOST,
+    port: process.env.PG_PORT,
+    user: process.env.PG_USER,
+    password: process.env.PG_PASSWORD,
+    database: process.env.PG_NAME,
     ssl: {
       rejectUnauthorized: false,
     },
